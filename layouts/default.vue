58f856e1b8ae2c9c028739a07c3b6fcd30d74e4d
Jamie <jamiewong63@googlemail.com>
2020-08-07 18:50:23 +0100

    using $auth.user to check login status and display email if logged in

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <Nuxt />
-
     <div v-if="$auth.loggedIn">
+      {{ $auth.user.email }}
+      <button text>Logout</button>
       <!-- username -->
       <!-- logout button -->
     </div>
     <div v-else>
+      <button text to="/login">Login</button>
+      <button text to="/register">Register</button>
       <!-- login -->
       <!-- logout -->
     </div>
